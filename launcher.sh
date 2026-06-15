8a15973eea3d026c136d3c15ecb173c395e03ca7
JohnStares <wisdom8achor24@gmail.com>
2026-06-15 16:20:36 +0100

    The run.py is the entrance to the application launcher.sh is just a linux way of running the application

---
+++ launcher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "/home/john-stares/Desktop/VS Code/api_client"
+source venv/bin/activate
+python3 run.py
---
+++ run.py
@@ -0,0 +1,8 @@
+from src.gui import APIRequestGUI
+
+
+
+
+if __name__ == "__main__":
+	app = APIRequestGUI()
+	app.run()
